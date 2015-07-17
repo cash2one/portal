61b18a5102b71e5e4556c7a28166bdcdba212f0c
unknown <3203317@qq.com>
2015-07-17 15:23:08 +0800

    haha

---
+++ web/settings.js
@@ -0,0 +1,22 @@
+/*!
+ * zswhcb-portal
+ * Copyright(c) 2015 zswhcb-portal <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+module.exports = { 
+	cookieSecret: 'zswhcb-portal',
+	cdn: 'http://www.foreworld.net/js/',
+	corp: {
+		name: '河南正森文化传播有限公司',
+		website: 'http://www.iim.com.cn/'
+	}, db: {
+		name: 'zswhcb-portal',
+		id: 'zswhcb-portal',
+		host: '127.0.0.1',
+		port: 27017,
+		user: 'sa',
+		pass: 'xiang123'
+	}
+};
