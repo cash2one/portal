1584bae7a475add68449d725d7d3db5a449c5e27
unknown <3203317@qq.com>
2015-08-31 18:49:20 +0800

    haha

--- web/biz/page_position.js
+++ web/biz/page_position.js
@@ -9,6 +9,8 @@ var util = require('speedt-utils'),
 	mysql_util = util.mysql_util,
 	mysql = util.mysql;
 
+var EventProxy = require('eventproxy');
+
 var exports = module.exports;
 
 /**
@@ -18,7 +20,45 @@ var exports = module.exports;
  * @return
  */
 exports.findPositionsByPage = function(page_id, cb){
+	// ep
+	var ep = EventProxy.create('positions', 'positions_ext', function (positions, positions_ext){
+		for(var i in positions_ext){
+			var ext = positions_ext[i];
+			for(var j in positions){
+				var position = positions[j];
+				if(ext.PID === position.id){
+					position[ext._KEY] = ext._VALUE;
+					break;
+				}
+			}
+		}
+		cb(null, positions);
+	});
+
+	ep.fail(function (err){
+		next(err);
+	});
+
 	mysql_util.find(null, 'w_page_position', [['PAGE_ID', '=', page_id]], null, null, function (err, docs){
+		if(err) return ep.emit('error', err);
+		ep.emit('positions', docs);
+	});
+
+	this.findPositionExtsByPage(page_id, function (err, docs){
+		if(err) return ep.emit('error', err);
+		ep.emit('positions_ext', docs);
+	});
+};
+
+/**
+ * 获取扩展数据
+ *
+ * @param
+ * @return
+ */
+exports.findPositionExtsByPage = function(page_id, cb){
+	var sql = 'SELECT * FROM w_page_position_ext WHERE PID in (SELECT id FROM w_page_position WHERE PAGE_ID=?)';
+	mysql.query(sql, [page_id], function (err, docs){
 		if(err) return cb(err);
 		cb(null, docs);
 	});
